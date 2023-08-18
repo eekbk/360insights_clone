76109dcb4c221a97328f2dc80014de54c091401b
eekbk <eekeric@gmail.com>
2023-08-18 12:13:05 -0500

    feat: add modal and close onclick or scroll

--- app/components/ContactHeader.tsx
+++ app/components/ContactHeader.tsx
@@ -1,16 +1,38 @@
 'use client'
 
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 import Image from "next/image"
 
 export default function ContactHeader() {
+  const ref = useRef<HTMLInputElement>(null);
   const [isOpen, setIsOpen] = useState(false);
   const chevron: string = 'transition ease transform duration-300';
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleClose, true)
+    return () => window.removeEventListener('scroll', handleClose, true);
+  }, [])
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e: any) => {
+      if (isOpen && !ref.current?.contains(e.target)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', checkIfClickedOutside)
+
+    return () => {document.removeEventListener('mousedown', checkIfClickedOutside)}
+  }, [isOpen])
+
+  const handleClose = () => setIsOpen(false);
+
+
 
   return (
     <header className="flex justify-end text-xs font-light tracking-wide p-4">
       <div className='flex mr-6 hover:underline'>
-        <button className=''>Call U.S 1-866-684-2308</button>
+        <button className=''>Call U.S. 1-866-684-2308</button>
         <button
           className={`${chevron} ${
             isOpen
@@ -23,6 +45,14 @@ export default function ContactHeader() {
         </button>
       </div>
       <button className="hover:underline">Support</button>
+      {isOpen &&
+        <div
+        className="absolute py-7 pl-6 pr-11 tracking-widest bg-white top-11 rounded-lg z-[100] shadow-md"
+        // onScroll={handleClose}
+        >
+          <button className="hover:underline">Call U.K. +44 (0)1789 404020</button>
+        </div>
+      }
     </header>
     )
 }
