8b2b34695a0e7ce1aee46b16d24f17118d023d94
eekbk <eekeric@gmail.com>
2023-08-19 16:16:52 -0500

    chore: update import paths

--- app/components/FuchsiaButton.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-export default function FuchsiaButton({ text }: { text: string }) {
-  return (
-    <button className="bg-pink-600 text-white text-lg font-bold tracking-wider py-3 px-11 my-9 rounded-3xl">{text}</button>
-  )
-}
