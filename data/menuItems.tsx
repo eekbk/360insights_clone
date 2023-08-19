033961fc983b93c40f68f82e6fa208770f335000
eekbk <eekeric@gmail.com>
2023-08-19 16:18:08 -0500

    create: data for menu

---
+++ data/menuItems.tsx
@@ -0,0 +1,111 @@
+import { MenuCategory } from "@/app/_types/interfaces"
+
+export const products: MenuCategory[] = [
+  {
+    title: 'Products',
+    items: [
+      ['Incentives & Rewards', 'SPIFFs, points and loyalty programs'],
+      ['Fund Management', 'MDF and Co-Op advertising'],
+      ['Consumer Rebates', 'Cashback for customers'],
+      ['Go-to-Market Portals', 'Enable ecosystem members'],
+      ['B2B Rebates', 'Purchase incentives for businesses'],
+      ['Pricing & Promotions', 'Configure Quotes and Discounts']
+    ]
+  },
+  {
+    title: 'Expert Services',
+    items: [
+      ['Claim Audit'],
+      ['Payments'],
+      ['Program Engagement'],
+      ['Channel Marketing'],
+    ],
+  },
+  {
+    title: 'Platform',
+    items: [
+      ['Platform Overview'],
+      ['Partner Ecosystem'],
+      ['Security'],
+    ]
+  },
+]
+
+export const solutions: MenuCategory[] = [
+  {
+    title: 'By Use Case',
+    items: [
+      ['Win mindshare and market share'],
+      ['Increase partner engagement'],
+      ['Align channel sales and marketing'],
+      ['Incentivize learning and education'],
+    ]
+  },
+  {
+    title: 'By Industry',
+    items: [
+      ['Automotive'],
+      ['Consumer Durables'],
+      ['Life Sciences'],
+      ['Technology, Media, Telecom'],
+      ['Health Plans & Insurance']
+    ]
+  }
+]
+
+export const resources: MenuCategory[] = [
+  {
+    title: 'Featured Topics',
+    items: [
+      ['Ecosystem Management', 'Achieve Seamless Partner Orchestration and Collaboration'],
+      ['Recession Proof Your Business', 'Maintain Brand Loyalty Whatever The Economy'],
+      ['Motivate, Engage and Incentivize Sales', 'Promote Effective Revenue Generating Behaviors'],
+      ['Channel Marketing', 'Drive More Demand Through Your Channel'],
+      ['The 360insights Way', 'Learn What Makes 360 So Unique'],
+    ]
+  },
+  {
+    title: 'Resource Library',
+    items: [
+      ['All Resources'],
+      ['Case Studies'],
+      ['Help Sheets'],
+      ['Infographics'],
+      ['Insights'],
+      ['Reports'],
+      ['Videos'],
+    ],
+  },
+  {
+    title: 'Blog',
+    items: [
+      ['All topics'],
+      ['Incentives & Rewards'],
+      ['Consumer Rebates'],
+      ['B2B Rebates'],
+      ['Fund Management'],
+      ['Go-To-Market Portals'],
+    ]
+  },
+  {
+    title: 'Events',
+    items: [
+      ['All Events'],
+      ['Videocasts'],
+    ]
+  },
+]
+
+export const about: MenuCategory[] = [
+  {
+    title: '',
+    items: [
+      ['About 360insights'],
+      ['Meet Leadership'],
+      ['360cares'],
+      ['Careers'],
+      ['Newsroom'],
+      ['Become a Partner']
+    ]
+  }
+]
