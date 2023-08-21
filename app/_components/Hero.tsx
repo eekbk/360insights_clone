c150ad44f4cf368c3ca1f19edcc754635be36a31
eekbk <eekeric@gmail.com>
2023-08-21 01:25:29 -0500

    style: update flex characteristics

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -8,7 +8,7 @@ const headerUtils: string = 'animate-slideUp';
 
 export default function Hero() {
   const [headerIndex, setHeaderIndex] = useState(0)
-  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+  // const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
   useMemo(() => {
     if (headerIndex === headers.length) {
@@ -25,37 +25,41 @@ export default function Hero() {
   }, [headerIndex]);
 
   return (
-    <div className="bg-genericBanner bg-cover py-12 px-5 tracking-[0.1px] flex flex-col md:flex-row justify-center">
-      <div className='text-2xl font-bold md:w-1/2'>
-        <h1
-          className={`${headerUtils} ${headerIndex === 0
-              ? 'text-purple-600'
-              : 'hidden'}`}
-        >{headers[0]}</h1>
-        <h1
-          className={`${headerUtils} ${headerIndex === 1
-              ? 'text-pink-600'
-              : 'hidden'}`}
-        >{headers[1]}</h1>
-        <h1
-          className={`${headerUtils} ${headerIndex === 2
-              ? 'text-amber-600'
-              : 'hidden'}`}
-        >{headers[2]}</h1>
-        <h1
-          className={`${headerUtils} ${headerIndex === 3
-              ? 'text-lime-600'
-              : 'hidden'}`}
-        >{headers[3]}</h1>
-        <h1>Without the Worry</h1>
-        <p className="text-xl font-light bg-transparent my-3">Channel software to motivate partners and manage spend. You do you. We&apos;ll handle the rest.</p>
-      <FuchsiaButton text="Products and Platform" />
-      </div>
-      <div className="bg-cyan-500 relative rounded-[2.5rem] w-[17rem] h-[17rem] my-7">
-          <Image className='absolute left-6 w-[98%] bottom-0 z-20' src='/ladyNumberOne.png' width={400} height={400} alt="Lady Number One" />
-          <Image className="absolute top-8 left-9 z-10 w-7/12" src='/logoShape.svg' width={400} height={400} alt="logo" />
-          <Image className="absolute -bottom-11 right-0 w-3/4 z-30" src='/dots.svg' width={400} height={400} alt="dots" />
-          <Image className="absolute bottom-14 -right-14 w-5/12 z-40" src='/graphOverlay.png' width={400} height={400} alt="dots" />
+    <div className="w-full bg-genericBanner bg-blend-darken bg-cover bg-center pt-11 pb-20 md:py-12 tracking-wide lg:py-28 flex justify-center px-[1.15rem]">
+      <div className="flex flex-col md:flex-row justify-between items-center lg:min-w-[64rem] w-full max-w-[73rem]">
+        <div className='text-2xl font-bold md:text-3xl lg:text-[2.5rem] lg:leading-[3rem] flex flex-col'> {/** Title/desc/button container */}
+            <h1
+              className={`${headerUtils} ${headerIndex === 0
+                  ? 'text-purple-600'
+                  : 'hidden'}`}
+            >{headers[0]}</h1>
+            <h1
+              className={`${headerUtils} ${headerIndex === 1
+                  ? 'text-pink-600'
+                  : 'hidden'}`}
+            >{headers[1]}</h1>
+            <h1
+              className={`${headerUtils} ${headerIndex === 2
+                  ? 'text-amber-600'
+                  : 'hidden'}`}
+            >{headers[2]}</h1>
+            <h1
+              className={`${headerUtils} ${headerIndex === 3
+                  ? 'text-lime-600'
+                  : 'hidden'}`}
+            >{headers[3]}</h1>
+            <h1>Without the Worry</h1>
+            <p className="text-xl font-light bg-transparent my-3 lg:w-[28rem]">Channel software to motivate partners and manage spend. You do you. We&apos;ll handle the rest.</p>
+          <FuchsiaButton text="Products and Platform" />
+        </div>
+        <div className="relative flex md:w-1/2 self-start md:self-center justify-end lg:w-[32rem] md:mr-20"> {/** Picture container */}
+          <div className="bg-cyan-500 rounded-[2.5rem] aspect-square w-[18.5rem] md:w-[64%] my-7 lg:w-96" >
+            <Image className='absolute -right-4 md:right-16 w-[98%] aspect-square md:w-[73%] lg:w-[30rem] lg:right-24 bottom-7 z-20' src='/ladyNumberOne.png' width={800} height={800} alt="Lady Number One" />
+            <Image className="absolute top-14 left-9 z-10 w-7/12 lg:w-[11rem] lg:left-[17rem]" src='/logoShape.svg' width={400} height={400} alt="logo" />
+            <Image className="absolute aspect-square -bottom-4 right-0 w-[12.5rem] md:-bottom-6 z-30" src='/dots.svg' width={400} height={400} alt="dots" />
+            <Image className="absolute bottom-16 -right-14 w-[45%] z-40 lg:w-52" src='/graphOverlay.png' width={400} height={400} alt="graph" />
+          </div>
+        </div>
       </div>
     </div>
   )
