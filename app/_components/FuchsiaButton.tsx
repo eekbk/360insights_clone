82c41d9da7dc437073bca4426d48ff0a020c8376
eekbk <eekeric@gmail.com>
2023-08-21 01:14:26 -0500

    style: updated max-width on button"

--- app/_components/FuchsiaButton.tsx
+++ app/_components/FuchsiaButton.tsx
@@ -2,6 +2,6 @@
 
 export default function FuchsiaButton({ text }: { text: string }) {
   return (
-    <button className="bg-pink-600 text-white text-lg font-bold tracking-wider py-3 px-11 my-9 rounded-3xl">{text}</button>
+    <button className="bg-pink-600 text-white text-lg font-bold tracking-wider py-3 px-11 md:px-6 md:rounded-[2rem] my-9 rounded-3xl lg:max-w-xs">{text}</button>
   )
 }
