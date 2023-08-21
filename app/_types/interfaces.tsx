b1a5d951c5d1d185ad95c05fd0f98ac55cb31936
eekbk <eekeric@gmail.com>
2023-08-21 01:29:44 -0500

    chore: create CardInfo interface

--- app/_types/interfaces.tsx
+++ app/_types/interfaces.tsx
@@ -2,3 +2,10 @@ export interface MenuCategory {
   title: string,
   items: Array<Array<string>>,
 }
+
+export interface CardInfo {
+  category: string,
+  title: string,
+  callToAction: string,
+  imageUrl: string,
+}
