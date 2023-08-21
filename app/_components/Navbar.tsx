a7a92e428181608e73dd8196976daadece119217
eekbk <eekeric@gmail.com>
2023-08-21 01:29:07 -0500

    chore and style: menu is dynamically rendered, update style

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -5,6 +5,8 @@ import Hamburger from "./Hamburger";
 import Chevron from "./chevron";
 import MobileMenuModal from "./mobileMenuModal";
 import { products, solutions, resources, about } from "@/data/menuItems";
+import DesktopMenuItem from "./desktopMenuItem";
+import MobileMenuItem from "./mobileMenuItem";
 
 const menuItems: string[] = ['Products', 'Solutions', 'Resources', 'About'];
 
@@ -15,6 +17,7 @@ export default function Navbar() {
   const [isSolutionsOpen, setIsSolutionsOpen] = useState(false);
   const [isResourcesOpen, setIsResourcesOpen] = useState(false);
   const [isAboutOpen, setIsAboutOpen] = useState(false);
+  const [isAnyMenuOpen, setIsAnyMenuOpen] = useState(false);
 
   const mobileMenuClasses: string = 'flex justify-between py-6 cursor-pointer hover:underline active:underline';
   const desktopMenuClasses: string = 'flex text-sm font-semibold [&>*]:mx-1'
@@ -51,59 +54,39 @@ export default function Navbar() {
     }
   }, [isAboutOpen])
 
+  useEffect(() => {
+    if(!isHamburgerOpen) {
+      setIsProductsOpen(false);
+      setIsSolutionsOpen(false);
+      setIsResourcesOpen(false);
+      setIsAboutOpen(false);
+    }
+  }, [isHamburgerOpen])
+
   return (
-    <nav className="top-0 sticky bg-white z-[100] overscroll-none">
-      <div className="w-full flex justify-between px-5 py-2">
+    <nav className="top-0 sticky bg-white z-[100] flex flex-col lg:flex-row justify-center">
+      <div className="w-full flex justify-between px-5 py-2 max-w-[73rem]">
         <div className="">
-        <Image src="https://www.360insights.com/hubfs/_2022/logo.svg" width={190} height={48} alt="360insights logo" />
+          <Image src="https://www.360insights.com/hubfs/_2022/logo.svg" width={190} height={48} alt="360insights logo" />
         </div>
           <Hamburger isOpen={isHamburgerOpen} setIsOpen={setIsHamburgerOpen} />
         <div className="hidden lg:flex justify-between items-center w-[42rem]">
-          <div className={desktopMenuClasses} >
-            <div>Products</div>
-            <Chevron isOpen={isProductsOpen} setIsOpen={setIsProductsOpen} size="medium"/>
-          </div>
-          <div className={desktopMenuClasses}>
-            <div>Solutions</div>
-            <Chevron isOpen={isSolutionsOpen} setIsOpen={setIsSolutionsOpen} size="medium"/>
-          </div>
-          <div className={desktopMenuClasses}>
-            <div>Resources</div>
-            <Chevron isOpen={isResourcesOpen} setIsOpen={setIsResourcesOpen} size="medium"/>
-          </div>
-          <div className={desktopMenuClasses}>
-            <div>About</div>
-            <Chevron isOpen={isAboutOpen} setIsOpen={setIsAboutOpen} size="medium" />
-          </div>
-        <button className="bg-cyan-600 text-white text-lg font-bold tracking-wider leading-5 py-3.5 px-6 rounded-[2rem]">Talk to Sales</button>
+          <DesktopMenuItem title="Products" isOpen={isProductsOpen} setIsOpen={setIsProductsOpen} section={products} />
+          <DesktopMenuItem title="Solutions" isOpen={isSolutionsOpen} setIsOpen={setIsSolutionsOpen} section={solutions} />
+          <DesktopMenuItem title="Resources" isOpen={isResourcesOpen} setIsOpen={setIsResourcesOpen} section={resources} />
+          <DesktopMenuItem title="About" isOpen={isAboutOpen} setIsOpen={setIsAboutOpen} section={about} />
+          <button className="bg-cyan-600 text-white text-lg font-bold tracking-wider leading-5 py-3.5 px-6 rounded-[2rem]">Talk to Sales</button>
         </div>
-
       </div>
-        {isHamburgerOpen &&
-          <div className="bg-white w-full flex flex-col px-5 text-sm font-semibold lg:hidden overscroll-none overflow-y-auto">
-            <div className={mobileMenuClasses} onClick={() => {setIsProductsOpen(!isProductsOpen)}}>
-              <div>Products</div>
-              <Chevron isOpen={isProductsOpen} setIsOpen={setIsProductsOpen} />
-            </div>
-            {isProductsOpen && <MobileMenuModal section={products} />}
-            <div className={mobileMenuClasses} onClick={() => {setIsSolutionsOpen(!isSolutionsOpen)}}>
-              <div>Solutions</div>
-              <Chevron isOpen={isSolutionsOpen} setIsOpen={setIsSolutionsOpen} />
-            </div>
-              {isSolutionsOpen && <MobileMenuModal section={solutions} />}
-            <div className={mobileMenuClasses} onClick={() => {setIsResourcesOpen(!isResourcesOpen)}}>
-              <div>Resources</div>
-              <Chevron isOpen={isResourcesOpen} setIsOpen={setIsResourcesOpen} />
-            </div>
-              {isResourcesOpen && <MobileMenuModal section={resources} />}
-            <div className={mobileMenuClasses} onClick={() => {setIsAboutOpen(!isAboutOpen)}}>
-              <div>About</div>
-              <Chevron isOpen={isAboutOpen} setIsOpen={setIsAboutOpen} />
-            </div>
-              {isAboutOpen && <MobileMenuModal section={about} />}
-            <button className="bg-cyan-600 text-white text-lg font-bold tracking-wider py-1.5 px-11 mt-2 mb-4 rounded-3xl">Talk to Sales</button>
-          </div>
-        }
+      {isHamburgerOpen &&
+        <div className="bg-white shadow-sm w-full flex flex-col px-5 text-sm font-semibold lg:hidden overflow-y-auto overscroll-none top-14">
+          <MobileMenuItem title='Products' isOpen={isProductsOpen} setIsOpen={setIsProductsOpen} section={products} />
+          <MobileMenuItem title='Solutions' isOpen={isSolutionsOpen} setIsOpen={setIsSolutionsOpen} section={solutions} />
+          <MobileMenuItem title='Resources' isOpen={isResourcesOpen} setIsOpen={setIsResourcesOpen} section={resources} />
+          <MobileMenuItem title='About' isOpen={isAboutOpen} setIsOpen={setIsAboutOpen} section={about} />
+          <button className="bg-cyan-600 text-white text-lg font-bold tracking-wider py-1.5 px-11 mt-2 mb-4 rounded-3xl">Talk to Sales</button>
+        </div>
+      }
     </nav>
   )
 }
