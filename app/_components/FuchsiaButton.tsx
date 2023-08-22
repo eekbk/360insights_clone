378182a5bac147b6342b3d8515f17f0e497cd100
eekbk <eekeric@gmail.com>
2023-08-22 00:26:28 -0500

    style: add active class to button

--- app/_components/FuchsiaButton.tsx
+++ app/_components/FuchsiaButton.tsx
@@ -2,6 +2,12 @@
 
 export default function FuchsiaButton({ text }: { text: string }) {
   return (
-    <button className="bg-pink-600 text-white text-lg font-bold tracking-wider py-3 px-11 md:px-6 md:rounded-[2rem] my-9 rounded-3xl md:max-w-xs">{text}</button>
+    <button
+      className="bg-pink-600 active:bg-cyan-360 text-white text-lg md:text-xl font-bold leading-8
+                tracking-wider py-3 px-11  md:rounded-[2rem] my-11 rounded-3xl
+                sm:w-fit"
+    >
+      {text}
+    </button>
   )
 }
