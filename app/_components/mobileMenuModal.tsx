6347cdb14176a89815b18ea7f6f06755fea3a5da
eekbk <eekeric@gmail.com>
2023-08-22 00:29:53 -0500

    style: update style on Mobile modal

--- app/_components/mobileMenuModal.tsx
+++ app/_components/mobileMenuModal.tsx
@@ -6,12 +6,12 @@ type Props = {
 
 export default function MobileMenuModal({section}: Props) {
   return (
-    <div className="bg-white overflow-y-scroll max-h-full lg:hidden overscroll-none">
+    <div className="bg-white lg:hidden h-full">
       {section.map(category => (
-        <div key={category.title} className="py-4 ">
+        <div key={category.title} className="py-4">
           <h1 className="text-cyan-600 text-lg font-bold tracking-wide">{category.title}</h1>
           {category.items.map(item => (
-            <div key={item[0]} className="ml-4 my-4">
+            <div key={item[0]} className="ml-4 my-5">
               <h2 className="font-semibold text-sm tracking-wide">{item[0]}</h2>
               {item[1] && <h3 className="font-normal text-gray-600 tracking-wide">{item[1]}</h3>}
             </div>
