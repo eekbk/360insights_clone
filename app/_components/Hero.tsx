f8575c8f32780f9b22d3e06b9eaf46838df5e640
eekbk <eekeric@gmail.com>
2023-08-22 00:28:03 -0500

    style: update styling on Hero, format classes

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -25,9 +25,10 @@ export default function Hero() {
   }, [headerIndex]);
 
   return (
-    <div className="w-full bg-genericBanner bg-blend-darken bg-cover bg-center pt-11 pb-20 md:py-12 tracking-wide lg:py-28 flex justify-center px-[1.15rem]">
-      <div className="flex flex-col md:flex-row justify-between items-center lg:min-w-[64rem] w-full max-w-[73rem]">
-        <div className='text-2xl font-bold md:text-3xl lg:text-[2.5rem] lg:leading-[3rem] flex flex-col'> {/** Title/desc/button container */}
+    <div className="w-full bg-genericBanner bg-blend-darken bg-cover bg-center pt-[50px] pb-20 md:py-12 tracking-wide lg:py-28 flex justify-center px-[1.15rem]">
+      <div className="flex flex-col md:flex-row justify-between items-center lg:min-w-[64rem] w-full max-w-[73rem]"> {/** Hero immediate wrapper */}
+        <div className='text-2xl font-bold md:text-3xl md:w-1/2 lg:text-[2.5rem] lg:leading-[3rem] flex flex-col justify-between'> {/** Title/desc/button container */}
+          <div> {/**Title/desc container */}
             <h1
               className={`${headerUtils} ${headerIndex === 0
                   ? 'text-purple-600'
@@ -49,15 +50,16 @@ export default function Hero() {
                   : 'hidden'}`}
             >{headers[3]}</h1>
             <h1>Without the Worry</h1>
+            </div>
             <p className="text-xl font-light bg-transparent my-3 lg:w-[28rem]">Channel software to motivate partners and manage spend. You do you. We&apos;ll handle the rest.</p>
           <FuchsiaButton text="Products and Platform" />
         </div>
-        <div className="relative flex md:w-1/2 self-start md:self-center justify-end lg:w-[32rem] md:mr-20"> {/** Picture container */}
-          <div className="bg-cyan-500 rounded-[2.5rem] aspect-square w-[18.5rem] md:w-[64%] my-7 lg:w-96" >
-            <Image className='absolute -right-4 md:right-16 w-[98%] aspect-square md:w-[73%] lg:w-[30rem] lg:right-24 bottom-7 z-20' src='/ladyNumberOne.png' width={800} height={800} alt="Lady Number One" />
+        <div className="relative flex md:w-[32rem] self-start md:self-center justify-end md:mr-2"> {/** Picture container */}
+          <div className="bg-cyan-500 rounded-[2.5rem] aspect-square w-[18.5rem] md:w-[15rem] my-5 lg:w-96" >
+            <Image className='absolute -right-4 md:right-16 w-[98%] aspect-square md:w-[97%] md:max-w-[282px] lg:w-[30rem] lg:max-w-none lg:right-24 bottom-5 z-20' src='/ladyNumberOne.png' width={800} height={800} alt="Lady Number One" />
             <Image className="absolute top-14 left-9 z-10 w-7/12 lg:w-[11rem] lg:left-[17rem]" src='/logoShape.svg' width={400} height={400} alt="logo" />
             <Image className="absolute aspect-square -bottom-4 right-0 w-[12.5rem] md:-bottom-6 z-30" src='/dots.svg' width={400} height={400} alt="dots" />
-            <Image className="absolute bottom-16 -right-14 w-[45%] z-40 lg:w-52" src='/graphOverlay.png' width={400} height={400} alt="graph" />
+            <Image className="absolute bottom-16 -right-14 w-[45%] z-40 md:w-48 lg:w-52" src='/graphOverlay.png' width={400} height={400} alt="graph" />
           </div>
         </div>
       </div>
