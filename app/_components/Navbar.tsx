4d96ebcdf74e1f5d657a1d1147d6a58b6c454d22
eekbk <eekeric@gmail.com>
2023-08-22 00:31:00 -0500

    style: update Navbar style and scroll behavior

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import Hamburger from "./Hamburger";
 import Chevron from "./chevron";
 import MobileMenuModal from "./mobileMenuModal";
@@ -63,28 +64,40 @@ export default function Navbar() {
     }
   }, [isHamburgerOpen])
 
+  const closeAll = () => {
+    setIsHamburgerOpen(false);
+    setIsProductsOpen(false);
+    setIsSolutionsOpen(false);
+    setIsResourcesOpen(false);
+    setIsAboutOpen(false);
+  }
+
   return (
-    <nav className="top-0 sticky bg-white z-[100] flex flex-col lg:flex-row justify-center">
+    <nav className="top-0 bg-white z-[100] flex flex-col lg:flex-row
+                justify-center relative h-full">
       <div className="w-full flex justify-between px-5 py-2 max-w-[73rem]">
-        <div className="">
+        <Link href="/" onClick={closeAll}>
           <Image src="https://www.360insights.com/hubfs/_2022/logo.svg" width={190} height={48} alt="360insights logo" />
-        </div>
+        </Link >
           <Hamburger isOpen={isHamburgerOpen} setIsOpen={setIsHamburgerOpen} />
         <div className="hidden lg:flex justify-between items-center w-[42rem] relative">
           <DesktopMenuItem title="Products" isOpen={isProductsOpen} setIsOpen={setIsProductsOpen} section={products} />
           <DesktopMenuItem title="Solutions" isOpen={isSolutionsOpen} setIsOpen={setIsSolutionsOpen} section={solutions} />
           <DesktopMenuItem title="Resources" isOpen={isResourcesOpen} setIsOpen={setIsResourcesOpen} section={resources} />
           <DesktopMenuItem title="About" isOpen={isAboutOpen} setIsOpen={setIsAboutOpen} section={about} />
-          <button className="bg-cyan-600 text-white text-lg font-bold tracking-wider leading-5 py-3.5 px-6 rounded-[2rem]">Talk to Sales</button>
+          <button className="bg-cyan-360 hover:bg-pink-600 text-white text-lg font-bold tracking-wider leading-5 py-3.5 px-6 rounded-[2rem]">Talk to Sales</button>
         </div>
       </div>
       {isHamburgerOpen &&
-        <div className="bg-white shadow-sm w-full flex flex-col px-5 text-sm font-semibold lg:hidden overflow-y-auto overscroll-none top-14">
-          <MobileMenuItem title='Products' isOpen={isProductsOpen} setIsOpen={setIsProductsOpen} section={products} />
-          <MobileMenuItem title='Solutions' isOpen={isSolutionsOpen} setIsOpen={setIsSolutionsOpen} section={solutions} />
-          <MobileMenuItem title='Resources' isOpen={isResourcesOpen} setIsOpen={setIsResourcesOpen} section={resources} />
-          <MobileMenuItem title='About' isOpen={isAboutOpen} setIsOpen={setIsAboutOpen} section={about} />
-          <button className="bg-cyan-600 text-white text-lg font-bold tracking-wider py-1.5 px-11 mt-2 mb-4 rounded-3xl">Talk to Sales</button>
+        <div className="absolute left-0 right-0 bg-white max-h-mobileMenu shadow-sm w-full
+                        flex flex-col px-5 text-sm font-semibold lg:hidden
+                         top-16 overflow-y-auto"
+        >
+            <MobileMenuItem title='Products' isOpen={isProductsOpen} setIsOpen={setIsProductsOpen} section={products} />
+            <MobileMenuItem title='Solutions' isOpen={isSolutionsOpen} setIsOpen={setIsSolutionsOpen} section={solutions} />
+            <MobileMenuItem title='Resources' isOpen={isResourcesOpen} setIsOpen={setIsResourcesOpen} section={resources} />
+            <MobileMenuItem title='About' isOpen={isAboutOpen} setIsOpen={setIsAboutOpen} section={about} />
+            <button className="bg-cyan-360 hover:bg-pink-600 active:bg-pink-600 text-white text-lg font-bold tracking-wider py-1.5 px-11 mt-2 mb-4 rounded-3xl">Talk to Sales</button>
         </div>
       }
     </nav>
