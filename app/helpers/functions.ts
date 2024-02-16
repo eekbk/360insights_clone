01a84a12aecd44e991b23474d42d178ff54ad99a
eekbk <eekeric@gmail.com>
2024-02-16 00:19:52 -0600

    feat: add debounce func

---
+++ app/helpers/functions.ts
@@ -0,0 +1,13 @@
+export const debounce = <T extends (...args: any[]) => any>(func: T, wait: number): ((...args: Parameters<T>) => void) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+  return (...args: Parameters<T>): void => {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(later, wait);
+  };
+};
