adc5b8595b7d49e90b450a78942d570bcbbe2e4c
eekbk <eekeric@gmail.com>
2024-02-16 00:20:25 -0600

    chore: refactor and cleanup event listeners

--- app/_components/desktopMenuItem.tsx
+++ app/_components/desktopMenuItem.tsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect, Dispatch, SetStateAction, useState } from "react";
 import { MenuCategory } from "../_types/interfaces";
+import { debounce } from "../helpers/functions";
 import Chevron from "./chevron";
 import DesktopMenuModal from "./desktopMenuModal";
 
@@ -11,29 +12,35 @@ type Props = {
 }
 
 export default function DesktopMenuItem({title, isOpen, setIsOpen, section}: Props) {
-  const ref = useRef<HTMLDivElement>(null);
-  const [windowSize, setWindowSize] = useState(0);
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [windowSize, setWindowSize] = useState<number>(window.innerWidth);
 
+  // check for window resizing to add event listener
   useEffect(() => {
-    setWindowSize(window.innerWidth);
-  }, [window.innerWidth])
+    const handleResize = debounce(() => {
+      setWindowSize(window.innerWidth);
+    }, 250); // debounce to avoid excessive state changes or rerenders
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
 
 
-
+  // add event listeners to close dropdown if user clicks outside
   useEffect(() => {
-    if (windowSize && windowSize >= 1024) {
-      const checkIfClickedOutside = (e: any) => {
-        e.preventDefault();
-        if(isOpen && ref.current && !ref.current?.contains(e.target) && e.target.id !== title) {
-          setIsOpen(false);
-        }
+    const checkIfClickedOutside = (e: any) => {
+      if(isOpen && ref.current && !ref.current?.contains(e.target)) {
+        setIsOpen(false);
       }
-
+    }
+    if (windowSize >= 1024 && isOpen) {
       document.addEventListener('mousedown', checkIfClickedOutside);
 
       return () => {document.removeEventListener('mousedown', checkIfClickedOutside)}
     }
-  }, [isOpen, setIsOpen, title, windowSize])
+
+    return () => {}
+  }, [isOpen, title, windowSize])
 
 
 
